aace687f55a645c8790df574cb3be561b60dbfb0
Victoria <victoria97916@gmail.com>
2018-03-12 13:30:38 +0200

    update

--- app/Post.php
+++ app/Post.php
@@ -12,4 +12,13 @@ class Post extends Model
     {
     	return $this->hasMany(Comment::class);
     }
+
+    public function addComment($body)
+    {
+    	//Comment::create([
+    		//'body' => $body,
+    		//'post_id' => $this->id
+    	//]);
+    	$this->comments()->create(compact('body'));
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,5 +23,25 @@
 	            @endforeach
 	        </ul>
 		</div>
+
+		<hr>
+
+		<div class="card">
+			<div class="card-block">
+				<div class="container">
+					<form method="POST" action="/posts/{{ $post->id }}/comments">
+						{{ csrf_field() }}
+						<div class="form-group">
+							<textarea name="body" placeholder="Your comment here." class="form-control" required></textarea>
+						</div>
+						<div class="form-group">
+							<button type="submit" class="btn btn-primary">Add comment</button>
+						</div>
+					</form>
+
+					@include('layouts.errors')
+				</div>
+			</div>
+		</div>
 	</div>
 @endsection
